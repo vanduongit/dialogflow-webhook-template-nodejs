7888a87621ea49b3bebbc2eb4d1db3ac7cc74206
Duong Phan <duong.phan@go1.com>
2018-11-30 21:03:13 +0700

    06: add enrollment

--- functions/enrolment.js
+++ functions/enrolment.js
@@ -1,17 +1,21 @@
-
-const { baseUrl } = require('./auth');
-const { api } = require('./api');
+const { baseUrl } = require("./auth");
+const { api } = require("./api");
+const { validateUser } = require("./user");
 
 module.exports = {
-    enrol: (courseId, userId) => {
-        const url = baseUrl + '/v2/enrollments';
-        const data = {
-            lo_id: courseId,
-            parent_lo_id: 0,
-            parent_enrollment_id: 0,
-            user_id: userId,
-        };
+  enrol: (courseId, jwt) => {
+    return validateUser(jwt).then(res => {
+      const url = "/v2/enrollments";
+      const userId = Number(res.data.userId);
 
-        return api.post(url, data);
-    },
+      const data = {
+        lo_id: courseId,
+        parent_lo_id: 0,
+        parent_enrollment_id: 0,
+        user_id: userId
+      };
+      
+      return api.post(url, data);
+    });
+  }
 };
